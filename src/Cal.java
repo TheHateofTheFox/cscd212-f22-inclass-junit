5665e5178b029da2a8c95cd94a860c9350960cf6
Timbre Freeman <timbre@tftinker.tech>
2022-11-15 07:47:01 -0800

    setup done

--- src/Cal.java
+++ src/Cal.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Cal {
+public class Cal {
+
+    private final double num1;
+    private final double num2;
+
+    public Cal(final double num1, final double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double add() {
+        return 0.0; // TODO: add code
+    }
+
+
+    public double multiply() {
+        return 0.0; // TODO: add code
+    }
+
+    public double diff() {
+        return 0.0; // TODO: add code
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(final String[] args) {
+        System.out.println("add");
+        System.out.println("1 + 2 = " + new Cal(1, 2).add()); // 3
+        System.out.println("-11 + 2 = " + new Cal(-11, 2).add()); // -9
+        System.out.println("multiply");
+        System.out.println("3 * 1 = " + new Cal(3, 1).multiply()); // 3
+        System.out.println("5 * 5 = " + new Cal(5, 5).multiply()); // 25
+        System.out.println("diff");
+        System.out.println("|2 - 3| = " + new Cal(2, 3).diff()); // 1
+        System.out.println("|6 - 6| = " + new Cal(6, 6).diff()); // 0
     }
 }
